3bd17f7f9129e3e2d0be07ed73df813b842715c1
Erik Olseng <e.olseng@gmail.com>
2020-12-08 15:33:18 +0100

    Extract AMQP declarations to utils module

---
+++ utils/src/main/kotlin/no/ID10022/pg6102/utils/amqp/amqp.kt
@@ -0,0 +1,9 @@
+package no.id10022.pg6102.utils.amqp
+
+/**
+ * Declaration of Exchanges, Routing Keys and Queues for AMQP / RabbitMQ
+ */
+
+// Auth service
+const val authExchangeName = "travel-agency.auth.dx"
+const val newUserRK = "new_user"
