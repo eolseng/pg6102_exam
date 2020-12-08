5026a2813010e86a5a22f92b4b5bdc05ab42a44a
Erik Olseng <e.olseng@gmail.com>
2020-12-08 14:35:15 +0100

    Add utils module

---
+++ utils/src/main/kotlin/no/ID10022/pg6102/utils/rest/PageDto.kt
@@ -0,0 +1,17 @@
+package no.ID10022.pg6102.utils.rest
+
+import io.swagger.annotations.ApiModel
+import io.swagger.annotations.ApiModelProperty
+import javax.validation.constraints.NotNull
+
+@ApiModel(description = "Paginated list of resources")
+data class PageDto<T>(
+
+        @ApiModelProperty("List of data contained in the page")
+        @get:NotNull
+        var list: List<T> = listOf(),
+
+        @ApiModelProperty("Link to the next page")
+        var next: String? = null
+
+)
